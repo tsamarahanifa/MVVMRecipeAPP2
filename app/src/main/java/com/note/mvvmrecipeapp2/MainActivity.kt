adc20d71cc196321342e4381f784e0942d9018fd
Tsamara Hanifa <tsamarahanifa@gmail.com>
2021-09-02 09:27:46 +0700

    jetpack compose setup for as

--- app/src/main/java/com/note/mvvmrecipeapp2/MainActivity.kt
+++ app/src/main/java/com/note/mvvmrecipeapp2/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.note.mvvmrecipeapp2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { 
-            Text(text = "HAI")
-        }
+       setContent {
+           Column (
+               modifier = Modifier.padding(16.dp)
+           ) {
+               Text(text = "HAI")
+               Spacer(modifier = Modifier.padding(top=10.dp))
+               Button(
+                   onClick = {}
+               ) {
+                   Text(text = "A BUTTON")
+               }
+           }
+       }
     }
 }
